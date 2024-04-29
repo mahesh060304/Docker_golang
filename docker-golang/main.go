8ef6bcf3deba9b682cea307a32cad260c9857ef9
mahesh060304 <107359538+mahesh060304@users.noreply.github.com>
2024-04-29 17:29:31 +0530

    Update main.go

--- docker-golang/main.go
+++ docker-golang/main.go
@@ -11,7 +11,7 @@ import (
     "go.mongodb.org/mongo-driver/mongo"
     "go.mongodb.org/mongo-driver/mongo/options"
     "github.com/go-redis/redis/v8"
-
+    "go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User struct {
@@ -22,6 +22,7 @@ type User struct {
 var RedisClient *redis.Client
 
 func main() {
+    // Set client options
     clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 
     // Connect to MongoDB
@@ -30,6 +31,7 @@ func main() {
         log.Fatal(err)
     }
 
+    // Ping the MongoDB server
     err = client.Ping(context.Background(), nil)
     if err != nil {
         log.Fatal(err)
@@ -37,8 +39,6 @@ func main() {
 
     fmt.Println("Connected to MongoDB!")
     collection := client.Database("docker").Collection("users")
-
-	//redis connection
     RedisClient = redis.NewClient(&redis.Options{
         Addr:     "localhost:6379",
         Password: "", // No password
@@ -59,6 +59,7 @@ func main() {
         return
     }
 
+    // Insert the new user into MongoDB
     _, err := collection.InsertOne(context.Background(), newUser)
     if err != nil {
         c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
@@ -75,6 +76,7 @@ func main() {
         }
     }
 
+    // Respond with success message
     c.JSON(200, gin.H{"message": "User created successfully"})
     })
 
@@ -91,6 +93,7 @@ func main() {
             return
         }
 
+    // Find all users in MongoDB
         cursor, err := collection.Find(context.Background(), bson.M{})
         if err != nil {
             c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
@@ -98,6 +101,7 @@ func main() {
         }
         defer cursor.Close(context.Background())
 
+    // Iterate over the cursor and store users in a slice
         var users []User
         for cursor.Next(context.Background()) {
             var user User
@@ -108,12 +112,45 @@ func main() {
             users = append(users, user)
         }
 
+        //Cache the users in Redis for 1 hour
         jsonData, err := bson.MarshalExtJSON(users, true, false)
         if err == nil {
             RedisClient.Set(context.Background(), "users", jsonData, time.Hour)
         }
 
+    // Send the users slice in the response
         c.JSON(http.StatusOK, users)
+    })
+
+    r.DELETE("/delete/:id",func(c *gin.Context) {
+        id :=c.Param("id")
+		objectID, err := primitive.ObjectIDFromHex(id)
+		if err!=nil{
+			log.Println("Error with objectid:", err)
+			return 
+		}
+
+
+		result, err := collection.DeleteMany(context.Background(), bson.M{"_id":objectID})
+        log.Println("Delete result:", result)
+
+		if err != nil {
+			log.Println("Error deleting users:", err)
+			return 
+		}
+
+		if result.DeletedCount == 0{
+			log.Println("Delete Count:", result.DeletedCount)
+
+			c.JSON(400, gin.H{"error":"User Not Found"})
+			return 
+		}
+
+		cacheKey := "user:" + id
+		if err := RedisClient.Del(context.Background(), cacheKey).Err(); err != nil {
+			log.Println("Error invalidating cache:", err)
+		}
+		c.JSON(200, gin.H{"message": "User deleted successfully"})
     })
     r.Run(":8081")    
 }
